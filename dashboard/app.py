d9e48c40cd6702c7e93fbfa7a44672aeec2e77dd
hendritedjo <hendri.tedjo@gmail.com>
2021-05-31 16:55:23 +0700

    dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -41,6 +41,7 @@ def result():
     if request.method == 'POST':
         input = request.form
         #id = int(input['angka'])
+        '''
         lim = int(input['limit_bal'])
         sex = int(input['sex'])
         ed = int(input['education'])
@@ -64,8 +65,44 @@ def result():
         pay4 = int(input['pay_amt4'])
         pay5 = int(input['pay_amt5'])
         pay6 = int(input['pay_amt6'])
+        '''
         #prediksi = Model.predict([[id,sex,ed,mar,pmt0,pmt2,pmt3,pmt4,pmt5,pmt6,bill1,bill2,bill3,bill4,bill5,bill6,pay1,pay2,pay3,pay4,pay5,pay6]])[0]
-        hasil = pd.DataFrame(data=[input], columns=['limit_bal', 'sex', 'education', 'marriage', 'age', 'pay_0', 'pay_2', 'pay_3', 'pay_4', 'pay_5', 'pay_6', 'bill_amt1', 'bill_amt2', 'bill_amt3', 'bill_amt4', 'bill_amt5', 'bill_amt6', 'pay_amt1', 'pay_amt2', 'pay_amt3', 'pay_amt4', 'pay_amt5', 'pay_amt6'])
+        
+        simpan = [
+            int(input['age']),
+            str(input['job']),
+            str(input['mar']),
+            str(input['edu']),
+            str(input['cre']),
+            str(input['hou']),
+            str(input['per']),
+            str(input['con']),
+            str(input['mon']),
+            str(input['day']),
+            int(input['pdays']),
+            int(input['prev']),
+            str(input['pout']),
+            float(input['var']),
+            float(input['pri']),
+            float(input['conf']),
+            float(input['eur']),
+            float(input['num'])
+        ]
+
+        hasil = pd.DataFrame(data=[simpan], columns=['age', 'job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'pdays', 'previous', 'poutcome', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed'])
+        
+        
+        hasil['education'] = hasil['education'].map({'basic.4y':1
+                              , 'basic.6y':2
+                              , 'basic.9y' :3
+                              , "high.school":4
+                              , "university.degree":5
+                              , 'professional.course':6
+                              , 'illiterate':0
+                              , 'unknown':0
+                             })
+        print(hasil)
+        '''
         df = pd.read_excel('CreditCard.xls')
         a = df.iloc[0]
         df = df[1:]
@@ -73,50 +110,134 @@ def result():
         df.set_index('ID', inplace=True)
         df.columns = df.columns.str.lower()
         df = df.astype('int')
-        hasil = hasil.astype('int')
+        '''
+        # hasil = hasil.astype('int')
         # print(hasil.dtypes)
-        df.drop(columns='default payment next month', inplace=True)
+        # df.drop(columns='default payment next month', inplace=True)
         #print(df.dtypes)
-        df = pd.concat([df,hasil], axis=0)
+        # df = pd.concat([df,hasil], axis=0)
         # print(df.dtypes)
-        df['education'][df['education'] == 0] = 4
-        df['education'][df['education'] >=4] = 4
-        df['marriage'][df['marriage'] == 0] = 3
-        df['pay_0'][df['pay_0'] == -2] = -1
-        df['pay_2'][df['pay_2'] == -2] = -1
-        df['pay_3'][df['pay_3'] == -2] = -1
-        df['pay_4'][df['pay_4'] == -2] = -1
-        df['pay_5'][df['pay_5'] == -2] = -1
-        df['pay_6'][df['pay_6'] == -2] = -1
-        df = pd.get_dummies(data=df, columns=['sex', 'education', 'marriage', 'pay_0', 'pay_2', 'pay_3', 'pay_4', 'pay_5', 'pay_6'])
-        print(df.columns)
-        hasil = df.iloc[[30000]]
         prediksi = Model.predict(hasil)
-    return render_template('result.html', data=input, pred=prediksi)
+        if prediksi == 1:
+            prediksi = 'Berhasil'
+        else:
+            prediksi = 'Ditolak'
+        prob = Model.predict_proba(hasil)
+        prob0 = round(prob[0][0], 2)
+        prob1 = round(prob[0][1], 2)
+    return render_template('result.html', data=input, pred=prediksi, proba=[prob0, prob1])
 
 '''
- <br>Limit Balance : <input type="number" step="1000" name="limit_bal">
-        <br>Sex : <input type="number" name="sex">
-        <br>Education : <input type="number" name="education">
-        <br>Marriage : <input type="number" name="marriage">
-        <br>Payment 0 : <input type="number" name="pay_0">
-        <br>Payment 2 : <input type="number" name="pay_2">
-        <br>Payment 3 : <input type="number" name="pay_3">
-        <br>Payment 4 : <input type="number" name="pay_4">
-        <br>Payment 5 : <input type="number" name="pay_5">
-        <br>Payment 6 : <input type="number" name="pay_6">
-        <br>Bill Amount 1 : <input type="number" name="bill_amt1">
-        <br>Bill Amount 2 : <input type="number" name="bill_amt2">
-        <br>Bill Amount 3 : <input type="number" name="bill_amt3">
-        <br>Bill Amount 4 : <input type="number" name="bill_amt4">
-        <br>Bill Amount 5 : <input type="number" name="bill_amt5">
-        <br>Bill Amount 6 : <input type="number" name="bill_amt6">
-        <br>Payment Amount 1 : <input type="number" name="bill_amt1">
-        <br>Payment Amount 2 : <input type="number" name="bill_amt2">
-        <br>Payment Amount 3 : <input type="number" name="bill_amt3">
-        <br>Payment Amount 4 : <input type="number" name="bill_amt4">
-        <br>Payment Amount 5 : <input type="number" name="bill_amt5">
-        <br>Payment Amount 6 : <input type="number" name="bill_amt6">
+<br>Age : <input type="number" step="1" name="age">
+        <br>Job : 
+        <select name="job">
+          <option value="housemaid">Housemaid</option>
+          <option value="services">Services</option>
+          <option value="admin.">Admin</option>
+          <option value="blue-collar">Blue-Collar</option>
+          <option value="technician">Technician</option>
+          <option value="retired">Retired</option>
+          <option value="management">Management</option>
+          <option value="unemployed">Technician</option>
+          <option value="self-employed">Self-employed</option>
+          <option value="unknown">Unknown</option>
+          <option value="entrepreneur">Entepreneur</option>
+          <option value="student">Student</option>
+        </select> 
+
+        <br>Marital : 
+        <select name="mar">
+          <option value="married">Married</option>
+          <option value="single">Single</option>
+          <option value="divorced">Divorced</option>
+          <option value="unknown">Unknown</option>
+        </select> 
+
+        <br>Education : 
+        <select name="edu">
+          <option value="illiterate">Illiterate</option>
+          <option value="basic.4y">Basic 4 Years</option>
+          <option value="basic.6y">Basic 6 Years</option>
+          <option value="basic.9y">Basic 9 Years</option>
+          <option value="high.school">High School</option>
+          <option value="university.degree">University Degree</option>
+          <option value="professional.course">Professional Course</option>
+          <option value="unknown">Unknown</option>
+        </select> 
+
+        <br>Credit in Default :
+        <select name="cre">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          <option value="unknown">Unknown</option>
+        </select> 
+
+        <br>Housing Loan :
+        <select name="hou">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          <option value="unknown">Unknown</option>
+        </select> 
+
+        <br>Personal Loan :
+        <select name="per">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          <option value="unknown">Unknown</option>
+        </select> 
+
+        <br>Contact Communication Type :
+        <select name="con">
+          <option value="telephone">Telephone</option>
+          <option value="cellular">Cellular</option>
+        </select> 
+
+        <br>Month (Last Contact ) : 
+        <select name="mon">
+          <option value="mar">March</option>
+          <option value="apr">April</option>
+          <option value="may">May</option>
+          <option value="jun">June</option>
+          <option value="jul">July</option>
+          <option value="aug">August</option>
+          <option value="sep">September</option>
+          <option value="oct">October</option>
+          <option value="nov">November</option>
+          <option value="dec">December</option>
+        </select> 
+
+        <br>Day of Week (Last Contact) : 
+        <select name="day">
+          <option value="mon">Monday</option>
+          <option value="tue">Tuesday</option>
+          <option value="wed">Wednesday</option>
+          <option value="thu">Thursday</option>
+          <option value="fri">Friday</option>
+          <option value="sat">Saturday</option>
+          <option value="sun">Sunday</option>
+        </select> 
+
+        <br>Last Contact From Previous Campaign : <input type="number" name="pdays">
+
+        <br>Previous Number of Contact : <input type="number" name="prev">
+
+        <br>Outcome from Previous Campaign : 
+        <select name="pout">
+          <option value="nonexistent">Nonexistent</option>
+          <option value="failure">Failure</option>
+          <option value="success">Success</option>
+        </select> 
+
+        <br>Employee Variation Rate : <input type="number" name="var">
+
+        <br>Consumer Price Index : <input type="number" name="pri">
+
+        <br>Consumer Confidence Index : <input type="number" name="conf">
+
+        <br>Euribor 3 Months Rate : <input type="number" name="eur">
+
+        <br>Number of Employees : <input type="number" name="num">
+
 '''
 
 
